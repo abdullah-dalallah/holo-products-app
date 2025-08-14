5c20d7a6917c6fd323f65d8b914c93944d87fb64
abdullhali53 <62836999+abdullah-dalallah@users.noreply.github.com>
2025-08-14 22:30:39 +0400

    feat: add local caching for products using SharedPreferences
    - Implemented ProductsLocalDataSource for storing and retrieving cached products
    - Integrated local data source into ProductsRepositoryImpl
    - Updated dependency injection to register local data source and SharedPreferences
    - Added CacheFailure handling in repository for offline scenarios

--- lib/features/products/data/repositories/products_repository_impl.dart
+++ lib/features/products/data/repositories/products_repository_impl.dart
@@ -3,27 +3,36 @@ import 'package:holo_products_app/core/error/error_strings/failures.dart';
 import 'package:holo_products_app/core/error/exceptions.dart';
 import 'package:holo_products_app/core/error/failures.dart';
 import 'package:holo_products_app/core/network/network_info.dart';
+import 'package:holo_products_app/features/products/data/data_sources/products_local_data_source.dart';
 import 'package:holo_products_app/features/products/data/data_sources/products_remote_data_source.dart';
 import 'package:holo_products_app/features/products/domain/entities/product_entity.dart';
 import 'package:holo_products_app/features/products/domain/repositories/products_repository.dart';
 
 class ProductsRepositoryImpl implements ProductsRepository{
   final ProductsRemoteDataSource remoteDataSource;
+  final ProductsLocalDataSource localDataSource;
   final NetworkInfo networkInfo;
 
-  ProductsRepositoryImpl(
-      {required this.remoteDataSource, required this.networkInfo});
+  ProductsRepositoryImpl({required this.remoteDataSource, required this.networkInfo,required this.localDataSource });
 
   @override
   Future<Either<Failure, List<ProductEntity>>> getProducts() async {
     if (await networkInfo.isConnected) {
       try {
-        final response = await remoteDataSource.getProducts();
-        return Right(response);
+        final remoteProducts = await remoteDataSource.getProducts();
+        await localDataSource.cacheProducts(remoteProducts);
+        return Right(remoteProducts);
       } on ServerException catch (serverex) {
         return Left(ServerFailure(errorObject: serverex.getErrorObject()));
       }
     } else {
+      print("Not connected");
+      try {
+        final localProducts = await localDataSource.getLastProducts();
+        return Right(localProducts);
+      } on CacheException {
+        return Left(CacheFailure(message: FailureMessages.CACHE_FAILURE_MESSAGE));
+      }
       return Left(OfflineFailure(message: FailureMessages.OFFLINE_FAILURE_MESSAGE));
     }
   }
